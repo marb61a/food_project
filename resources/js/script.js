9d2375202148a4ba835b6026b6f3b2731cb6b16e
marb <marb61a@gmail.com>
2015-10-05 12:23:31 +0000

    updating script.js

---
+++ resources/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  
+    // Sticky Navigation
+    $('.js--section-features').waypoint(function(direction){
+      if (direction == "down") {
+            $('nav').addClass('sticky');
+      } else{
+            $('nav').removeClass('sticky');
+      }
+      }, {
+      offset: '60px;'
+    });
+    
+  
+    // Scroll On Buttons
+    $('.js--scroll-to-plans').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    
+})
